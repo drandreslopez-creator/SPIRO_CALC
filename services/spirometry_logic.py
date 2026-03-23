43576fca99a75a885f15c19783b57dc401913941
drandreslopez-creator <dr.andreslopez@gmail.com>
2026-03-22 20:59:58 -0500

    Add files via upload

--- services/spirometry_logic.py
+++ services/spirometry_logic.py
@@ -1,5 +1,8 @@
+# services/spirometry_logic.py
+
 from dataclasses import dataclass
 from typing import Optional
+
 
 @dataclass
 class ParameterResult:
@@ -13,13 +16,25 @@ class ParameterResult:
     zscore_post: Optional[float]
 
     @property
-    def pct_pred_pre(self):
+    def pct_pred_pre(self) -> Optional[float]:
         if self.measured_pre is None or self.predicted in (None, 0):
             return None
         return (self.measured_pre / self.predicted) * 100
 
     @property
-    def pct_pred_post(self):
+    def pct_pred_post(self) -> Optional[float]:
         if self.measured_post is None or self.predicted in (None, 0):
             return None
         return (self.measured_post / self.predicted) * 100
+
+    @property
+    def delta_abs(self) -> Optional[float]:
+        if self.measured_pre is None or self.measured_post is None:
+            return None
+        return self.measured_post - self.measured_pre
+
+    @property
+    def delta_pct_baseline(self) -> Optional[float]:
+        if self.measured_pre in (None, 0) or self.measured_post is None:
+            return None
+        return ((self.measured_post - self.measured_pre) / self.measured_pre) * 100
