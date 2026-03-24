6f800cba4f326daac791ff4f588f91a62bc1eb4b
drandreslopez-creator <dr.andreslopez@gmail.com>
2026-03-24 14:13:23 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -114,33 +114,6 @@ def calcular_predichos_lln(rows_data: dict, edad: Optional[float], sexo: str, ta
 
     return rows_updated
 
-# ----------------------------
-# Clinical engine
-
-    @property
-    def pct_pred_pre(self) -> Optional[float]:
-        if self.measured_pre is None or self.predicted in (None, 0):
-            return None
-        return (self.measured_pre / self.predicted) * 100
-
-    @property
-    def pct_pred_post(self) -> Optional[float]:
-        if self.measured_post is None or self.predicted in (None, 0):
-            return None
-        return (self.measured_post / self.predicted) * 100
-
-    @property
-    def delta_abs(self) -> Optional[float]:
-        if self.measured_pre is None or self.measured_post is None:
-            return None
-        return self.measured_post - self.measured_pre
-
-    @property
-    def delta_pct_baseline(self) -> Optional[float]:
-        if self.measured_pre in (None, 0) or self.measured_post is None:
-            return None
-        return ((self.measured_post - self.measured_pre) / self.measured_pre) * 100
-
 
 # ----------------------------
 # Sidebar / static content
